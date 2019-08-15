de28a9e89084d2ed0892ff323f14b535f45b6cc4
Adam Hedges <achedges@gmail.com>
2019-08-14 19:46:56 -0500

    Added simple test function for file downloader

---
+++ filesystem/tests/TestDownloader.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class TestDownloader {
+	@Test
+	public void testFileDownload() {
+		String url = "https://www.quandl.com/api/v3/databases/ASINDU/download?api_key=fyGPkWbZ4ssvdsmUTH46&download_type=all-trading-days";
+		FileSysUtils.downloadFile(url, "all-trading-days.zip");
+	}
+}
