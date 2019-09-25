2d86dbccf012ab482d4a658fb4c1f37653fe054f
Adam Hedges <achedges@gmail.com>
2019-09-25 08:40:17 -0500

    Added DecimalUtils

---
+++ src/decimalutils/DecimalUtils.java
@@ -0,0 +1,18 @@
+package decimalutils;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+
+public class DecimalUtils {
+
+	public static double truncate(double input, int scale) {
+		return BigDecimal.valueOf(input).setScale(scale, RoundingMode.HALF_UP).doubleValue();
+	}
+
+	public static String formatCurrency(double input) {
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+		return formatter.format(input);
+	}
+
+}
